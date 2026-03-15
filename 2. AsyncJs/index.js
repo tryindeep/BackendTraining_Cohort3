113ed0dd08d7b146b6ffec09c7c27acf6e6ebf62
TryInDeep <164211616+TryInDeep@users.noreply.github.com>
2026-03-15 22:10:52 +0530

    1. Async JS Some topics 2. Promises, why promises are better than callback, callback hell

--- 2. AsyncJs/index.js
+++ 2. AsyncJs/index.js
@@ -1,5 +1,45 @@
-const fs = require ("fs");
+// const { log } = require("console");
+// const fs = require ("fs");
 
-const contents = fs.readFile("a.txt" , "utf-8");  // asynchronusly
-const contents1 = fs.readFileSync("b.txt" , "utf-8"); // synchronusly
-console.log(contents,contents1);
+
+// // function read(err , data){
+// //     console.log(data);
+// // }
+
+// // fs.readFile("a.txt" , "utf-8", read );  // async function 
+// // fs.readFileSync("b.txt" , "utf-8", read); // synchronusly
+
+// // console.log("Done !");
+
+
+// function timeout() {
+//   console.log("Done from Timeout");
+// }
+
+// console.log("Hi ");
+
+// setTimeout(timeout, 1000);
+
+// console.log("Welcome to loupe");
+
+// let c;
+// for (let i = 0; i <= 10000000000; i++) {
+//   c++;
+// }
+
+// console.log("Expensive Operation Done");
+//check here for visulization : How JS executes asynchronous code - http://latentflip.com/loupe/ 
+
+
+//  setTimeoutSync
+
+function setTimeoutSync (timeout){
+    let startTime = new Date();
+    while(1){
+        let currentTime = new Date();
+        if( currentTime.getTime() - startTime.getTime() > timeout)
+            break;
+    }
+}
+setTimeoutSync(1000);
+console.log("Hi there");
---
+++ 2. AsyncJs/promises.js
@@ -0,0 +1,102 @@
+// ==========Promises========== 
+
+// let p = new Promise ((resolve , reject) => {
+//     let a = 1+2;
+//     if(a == 2){
+//         resolve("Success");
+//     }else{
+//         reject("Failed");
+//     }
+// })  
+
+// p.then((massage) => {
+//     console.log("This is in the then " + massage);
+// }).catch((massage) => {
+//     console.log("This is in the catch " + massage);
+// })
+
+// --------------Callback and why promises are better than callbacks -----------
+
+//USING_CALLBACK
+// let userLeft = false;
+// let userWatchingCatMeme  = true;
+
+// function userWatchingCallbackTutorial (callback, errorCallback) {
+//     if(userLeft){
+//         errorCallback({
+//             why : " userLeft",
+//             massage :  ":)"
+//         })
+//     }else if (userWatchingCatMeme){
+//         errorCallback({
+//             why : " User watching cat meme",
+//             massage :  "cat > tryIndeep"
+//         })
+//     }else{
+//         callback("Learned callbacks")
+//     }
+// }
+
+// userWatchingCallbackTutorial((massage) => {
+//     console.log("Success : " + massage);
+// }, (error) => {
+//     console.log(error.why+ " " + error.massage);
+    
+// });
+
+// why promises are better than callbacks  version with same example?
+// [Callback hell], or the "pyramid of doom," is a JavaScript programming issue caused 
+//by excessively nesting multiple asynchronous callbacks, making code unreadable, hard to maintain, and difficult to debug.
+
+// let userLeft1 = true;
+// let userWatchingCatMeme1  = false;
+// function userWatchingCallbackPromise () {
+//     return new Promise ((resolve , reject) => {
+//          if(userLeft1){
+//         reject({
+//             why : " userLeft",
+//             massage :  ":)"
+//         })
+//     }else if (userWatchingCatMeme1){
+//         reject({
+//             why : " User watching cat meme",
+//             massage :  "cat > tryIndeep"
+//         })
+//     }else{
+//         resolve("Learned callbacks")
+//     }
+//     })
+   
+// }
+
+// userWatchingCallbackPromise().then((massage) => {
+//     console.log("Success : " + massage);
+// }).catch((error) => {
+//     console.log(error.why+ " " + error.massage);
+// });
+
+// Promises Method!
+
+const videoRecordOne = new Promise ((resolve , reject) => {
+    resolve("video 1 recorded")
+});
+const videoRecordTwo = new Promise ((resolve , reject) => {
+    resolve("video 2 recorded")
+});
+const videoRecordThree = new Promise ((resolve , reject) => {
+    resolve("video 3 recorded")
+});
+
+// Promise.all([
+//     videoRecordOne, 
+//     videoRecordTwo, 
+//     videoRecordThree]).then((massage) => {
+//         console.log(massage);
+//     })
+// who complete first that will print
+Promise.race([
+    videoRecordOne, 
+    videoRecordTwo, 
+    videoRecordThree]).then((massage) => {
+        console.log(massage);
+    })
