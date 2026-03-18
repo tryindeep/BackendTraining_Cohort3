d04ade53b64ba70e5cf57287c7f76c519a7c3546
TryInDeep <164211616+TryInDeep@users.noreply.github.com>
2026-03-18 23:18:28 +0530

    not done anything today!!

--- 2. AsyncJs/04.callbackhell.js
+++ 2. AsyncJs/04.callbackhell.js
@@ -7,3 +7,19 @@ setTimeout(function () {
     }, 5000);
   }, 3000);
 }, 1000);
+
+
+function setTimeoutPromisified(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function solve() {
+	await setTimeoutPromisified(1000);
+	console.log("hi");
+	await setTimeoutPromisified(3000);
+	console.log("hello");
+	await setTimeoutPromisified(5000);
+	console.log("hi there");
+}
+
+solve();
