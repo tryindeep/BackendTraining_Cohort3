3bc36067df6fe40bb4fe099d4274aa7462005286
TryInDeep <164211616+TryInDeep@users.noreply.github.com>
2026-05-21 13:03:29 +0530

    Lerned about SQL Transaction (Begin Commit)

--- 12.Postgres_and_SQL/dist/index.js
+++ 12.Postgres_and_SQL/dist/index.js
@@ -1,10 +1,40 @@
+import "dotenv/config";
 import { Client } from "pg";
-// const pgClient = new Client("postgresql://neondb_owner:npg_5xY1tzMTBcwq@ep-super-union-aqp0knxr-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
-const pgClient2 = new Client("postgresql://neondb_owner:npg_5xY1tzMTBcwq@ep-super-union-aqp0knxr-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
-async function main() {
-    await pgClient2.connect();
-    const response = await pgClient2.query("SELECT * FROM users");
-    console.log(response.rows);
-}
-main();
+import express from "express";
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3001;
+const pgClient = new Client(process.env.DATABASE_URL);
+pgClient.connect();
+app.post("/signup", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+    const city = req.body.city;
+    const country = req.body.country;
+    const street = req.body.street;
+    const pincode = req.body.pincode;
+    try {
+        const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id;`;
+        const addressInsertQuery = `INSERT INTO addresses (city, country, street, pincode, user_id) VALUES ($1, $2, $3, $4, $5);`;
+        await pgClient.query("BEGIN;");
+        const response = await pgClient.query(insertQuery, [username, email, password,]);
+        const userId = response.rows[0].id;
+        await new Promise(x => setTimeout(x, 100 * 1000)); // stop the control on this line for 100s
+        const addressInsertResponse = await pgClient.query(addressInsertQuery, [city, country, street, pincode, userId,]);
+        await pgClient.query("COMMIT;");
+        res.json({
+            massage: "You have signed",
+        });
+    }
+    catch (e) {
+        console.log(e);
+        res.json({
+            message: "Error  while signing Up",
+        });
+    }
+});
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+});
 //# sourceMappingURL=index.js.map
--- 12.Postgres_and_SQL/src/index.ts
+++ 12.Postgres_and_SQL/src/index.ts
@@ -1,20 +1,50 @@
-import {Client} from "pg";
+import "dotenv/config";
+import { Client } from "pg";
+import express from "express";
 
+const app = express();
+app.use(express.json());
 
-const pgClient2 = new Client("connection string");
+const port = process.env.PORT || 3001;
+const pgClient = new Client(process.env.DATABASE_URL);
+pgClient.connect();
 
-async function  main(){
-    await pgClient2.connect();
+app.post("/signup", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
 
-    const response = await pgClient2.query("SELECT * FROM users");
-    console.log(response.rows);
-}
+  const city = req.body.city;
+  const country = req.body.country;
+  const street = req.body.street;
+  const pincode = req.body.pincode;
 
+  try {
+    const insertQuery =
+      `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id;`;
+    const addressInsertQuery =
+      `INSERT INTO addresses (city, country, street, pincode, user_id) VALUES ($1, $2, $3, $4, $5);`;
 
-main();
+    await pgClient.query("BEGIN;");
+    const response = await pgClient.query(insertQuery, [username,email,password,]);
+    const userId = response.rows[0].id;
+    await new Promise( x => setTimeout(x, 100* 1000)) // stop the control on this line for 100s
+    const addressInsertResponse = await pgClient.query(addressInsertQuery, [city,country,street,pincode,userId,]);
 
+    await pgClient.query("COMMIT;");
+   
 
+    res.json({
+      massage: "You have signed",
+    });
+  } catch (e) {
+    console.log(e);
+    res.json({
+      message: "Error  while signing Up",
+    });
+  }
+});
 
-
-
-
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
