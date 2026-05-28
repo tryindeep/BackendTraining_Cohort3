dd799d80d8b4991b84f7e43b1cfd5cd1f04ec4bb
TryInDeep <164211616+TryInDeep@users.noreply.github.com>
2026-05-28 09:10:29 +0530

    next-new-learnings

---
+++ 15.NextJs/nextjs-part--3/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react"
+
+export default function ({children} : {
+    children : ReactNode
+}) {
+    return <div>
+        <div>Header</div>
+        {children}
+        <div>footer</div>
+    </div>
+}
